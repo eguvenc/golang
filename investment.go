cb9981cde72d6d0bf2368d3362a776f63e150164
eguvenc <eguvenc@gmail.com>
2024-10-18 12:23:08 +0300

    printf, sprintf and println

--- investment.go
+++ investment.go
@@ -9,7 +9,7 @@ func main() {
 	const inflationRate = 6.5
 	var investmentAmount float64
 	var years float64
-	var expectedReturnRate = 5.5
+	var expectedReturnRate float64 = 5
 
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
@@ -19,6 +19,11 @@ func main() {
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	var futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+
+	var formatedFeatureValue = fmt.Sprintf("Future value: %.1f\n", futureValue)
+	var formatedRealFeatureValue = fmt.Sprintf("Future value (Adjusted for Inlation): %.1f\n", futureRealValue)
+
+	// fmt.Printf("Future value: %.1f\nFuture value (Adjusted for Inlation): %.1f\n", futureValue, futureRealValue)
+	// fmt.Println("Future value (Adjusted for Inlation): ", futureRealValue)
+	fmt.Print(formatedFeatureValue, formatedRealFeatureValue)
 }
