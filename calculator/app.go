942a9aea12ee0f31b80b1f65eef656f538d2e44f
eguvenc <eguvenc@gmail.com>
2024-10-18 14:51:45 +0300

    updates

--- calculator/app.go
+++ calculator/app.go
@@ -3,25 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
 	var taxRate float64 = 25.8
+	revenue := getUserInput("Revenue : ")
+	expenses := getUserInput("Expenses: ")
+	ebt, profit, ratio := caluculateFinancials(revenue, expenses, taxRate)
 
-	fmt.Print("Revenue : ")
-	fmt.Scan(&revenue)
+	fmt.Printf("%.2f\n", ebt) //  %.2df means = add two decimal place end of the output
+	fmt.Printf("%.2f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+}
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+func caluculateFinancials(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
+}
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := float64(revenue - expenses)
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
-
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
-
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
--- investment.go
+++ investment.go
@@ -5,25 +5,37 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
-	const inflationRate = 6.5
 	var investmentAmount float64
 	var years float64
 	var expectedReturnRate float64 = 5
 
-	fmt.Print("Investment Amount: ")
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Number Of Years: ")
+	// fmt.Print("Number Of Years: ")
+	outputText("Number Of Years: ")
 	fmt.Scan(&years)
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	var futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
-
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 	var formatedFeatureValue = fmt.Sprintf("Future value: %.1f\n", futureValue)
 	var formatedRealFeatureValue = fmt.Sprintf("Future value (Adjusted for Inlation): %.1f\n", futureRealValue)
 
 	// fmt.Printf("Future value: %.1f\nFuture value (Adjusted for Inlation): %.1f\n", futureValue, futureRealValue)
 	// fmt.Println("Future value (Adjusted for Inlation): ", futureRealValue)
 	fmt.Print(formatedFeatureValue, formatedRealFeatureValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (futureValue float64, futureRealValue float64) {
+	futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
+
+	return futureValue, futureRealValue
 }
